d2e6dc3462eab2d37fedc1817f21e30419d8b75c
RJ <roljohn.villanueva@awsys-i.com>
2015-11-24 15:56:59 +0800

    Dummy commit from pulled project

---
+++ simpleclass/src/main/java/dummyrepo/project/ace/aws/com/simpleclass/armorider/Suvarious.java
@@ -0,0 +1,14 @@
+package dummyrepo.project.ace.aws.com.simpleclass.armorider;
+
+/**
+ * Created by roljohn.villanueva on 11/24/2015.
+ */
+public class Suvarious {
+    public static String NullSpell(int u){
+        if((u % 2) == 0)
+            return "I COUNTER!!! your spell.";
+        else
+            return "Good.";
+    }
+
+}
