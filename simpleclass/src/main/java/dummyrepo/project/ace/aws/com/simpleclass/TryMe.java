5e6b3b0ecba3eb39f4d40133144fa6e4281c9729
RJ <roljohn.villanueva@awsys-i.com>
2015-11-24 15:45:54 +0800

    Dummy Updated

---
+++ simpleclass/src/main/java/dummyrepo/project/ace/aws/com/simpleclass/TryMe.java
@@ -0,0 +1,9 @@
+package dummyrepo.project.ace.aws.com.simpleclass;
+
+/**
+ * Created by roljohn.villanueva on 11/24/2015.
+ */
+public class TryMe {
+    public static int u = 999;
+    public static String greeting = "Who YOU!?!?";
+}
